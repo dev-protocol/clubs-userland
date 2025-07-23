0790f03eff774da0e4591dddc22485585bb305d3
Aggre <hiroyuki.aggre@gmail.com>
2025-07-23 12:07:23 +0900

    disable `security.checkOrigin`

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { config } from 'dotenv'
 import { defineConfig } from 'astro/config'
 import vercel from '@astrojs/vercel/serverless'
 
- 
 config()
 
 export default defineConfig({
 	output: 'server',
 	adapter: vercel(),
+	security: {
+		checkOrigin: false,
+	},
 })

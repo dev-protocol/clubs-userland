d1b3cd5bd1559cf07fdddeaf69351bbbea2aa353
Aggre <hiroyuki.aggre@gmail.com>
2025-08-04 14:00:10 +0900

    Revert "use public providers"
    This reverts commit 1e64f1a826e62a73bda206fc24920fa8d61d74b6.

--- utils/providers.ts
+++
@@ -1,9 +0,0 @@
-import { FallbackProvider, JsonRpcProvider } from 'ethers'
-
-export const publicPolygonProvider = new FallbackProvider(
-	[
-		new JsonRpcProvider('https://polygon-rpc.com'),
-		new JsonRpcProvider('https://polygon-bor-rpc.publicnode.com'),
-	],
-	137, // Polygon mainnet chain ID
-)
